2f62fd76a8696f6eaf2c843f3808f6091421b485
Abhishek Anand <abhi@hyperscripts.io>
2025-10-24 18:16:15 +0530

    test

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'location-tracker-v3'; // Increment this each time!
+const CACHE_NAME = 'location-tracker-v4'; // Increment this each time!
 
 // Get the base path from the service worker's location
 const BASE_PATH = self.location.pathname.substring(0, self.location.pathname.lastIndexOf('/') + 1);
